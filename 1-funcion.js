e7c0a0aff9fc424f2c30c49e8a009d259cc97925
Consue <c.anguitasalinas@gmail.com>
2022-07-14 23:16:18 -0400

    feat: answers

---
+++ 1-funcion.js
@@ -0,0 +1,10 @@
+//1 función desafío
+/* 
+function example(a, b, c) {
+  return a + b + c
+} */
+
+//Respuesta
+let example = function(a, b, c) {
+  return a+b+c;
+} 
---
+++ 2-arrow.js
@@ -0,0 +1,7 @@
+// 2 función desafío
+/* suma = function (a, b) {
+  return a + b
+} */
+
+//Respuesta
+let suma = (a,b) => a + b;
